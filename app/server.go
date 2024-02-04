5665e72c21b667efb52df3104200f2ddbef4c57e
Gusti Alfian M. P <gustialfianmp@gmail.com>
2024-02-04 12:11:07 +0700

    stage 3

--- app/server.go
+++ app/server.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"fmt"
-
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
@@ -20,30 +15,30 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	handleCon(conn)
+		go HandleCon(conn)
+	}
 }
 
-func handleCon(conn net.Conn) {
-	defer conn.Close()
+func HandleCon(conn net.Conn) {
+	for {
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf)
+		if err != nil {
+			return
+		}
 
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		return
+		if string(buf[:n]) == "*1\r\n$4\r\nping\r\n" {
+			conn.Write([]byte("+PONG\r\n"))
+			return
+		}
+
+		conn.Write([]byte("unknown msg\r\n"))
 	}
-
-	// log.Println("Received data:", buf[:n], []byte("PING\n"))
-	if string(buf[:n]) == "*1\r\n$4\r\nping\r\n" {
-		conn.Write([]byte("+PONG\r\n"))
-		return
-	}
-
-	// Write the same data back
-	conn.Write(buf[:n])
 }
